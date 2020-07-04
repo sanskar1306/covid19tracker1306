2f864aea05e9c6f967ea8a7a2338588bcac41f42
RONAN <sanskarkabra1306@gmail.com>
2020-07-04 16:44:12 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./components/Globalstyle";
+import { lightTheme, darkTheme } from "./components/Themes"
+
 import './App.css';
 import {Data} from './components/dat';
-import Covid from './covid.png'
+
 
 function App() {
- 
+  
+  const [theme, setTheme] = useState('light');
+    
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+}
+  
   return (
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+    <>
+    <GlobalStyles/>
     <div className="App">
       <header className="App-header">
        <h1>COVID19 TRACKER</h1>
+       <button onClick={themeToggler} className="butDar">Switch Theme</button>
       </header>
-      <Data />
+       <Data />
       
       
     </div>
+    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Themes.js
@@ -0,0 +1,12 @@
+export const lightTheme = {
+    body: 'cyan',
+    text: '#363537',
+    toggleBorder: '#FFF',
+    background: '#363537',
+}
+export const darkTheme = {
+    body: '#363537',
+    text: '#363537',
+    toggleBorder: '#6B8096',
+    background: '#999',
+}
